6493107243662ee350cfac086c54323055052416
luke <jjyjjh33@gmail.com>
2024-04-05 15:10:24 +0900

    chore: 메뉴 클릭 내/외부 판단 및 댓글 관련 몇가지 코드 수정

--- src/components/units/volunteer/detail/volunteerDetailHeader/VolunteerDetailHeader.container.js
+++ src/components/units/volunteer/detail/volunteerDetailHeader/VolunteerDetailHeader.container.js
@@ -1,10 +1,35 @@
 import { useRouter } from "next/router"
 import VolunteerDetailHeaderUI from "./VolunteerDetailHeader.presenter"
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function VolunteerDetailHeader(props) {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
   const router = useRouter();
   const status = router.query.name;
+  const wrapperRef = useRef(null);
+
+  //매뉴를 눌렀을 때 메뉴창이 나오도록 하는 기능 (헤더 컴포넌트에 넘겨줄값이다.)
+  const handleMenuClick = () => {
+    setIsMenuClicked(true);
+  };
+
+  useEffect(() => {
+    // 외부 클릭을 감지하는 함수
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        // 메뉴창 닫기
+        setIsMenuClicked(false);
+      }
+    }
+
+    // 이벤트 리스너 등록
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []); // 빈 배열을 넘겨 컴포넌트 마운트 시에만 실행되도록 함
 
   const PrevPage = () => {
     if (status == 'member' && router.pathname === "/volunteer/detail") {
@@ -32,9 +57,9 @@ export default function VolunteerDetailHeader(props) {
         isJoinedClikced={props.isJoinedClikced}
         title={title}
         PrevPage={PrevPage}
-        isMenuClicked={props.isMenuClicked}
-        handleMenuClick={props.handleMenuClick}
-        handleOutsideMenuClick={props.handleOutsideMenuClick}
+        isMenuClicked={isMenuClicked}
+        handleMenuClick={handleMenuClick}
+        wrapperRef={wrapperRef}
       />
     </>
   )
