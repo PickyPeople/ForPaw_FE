5876bacd72121fccfd231111aae28e1d4b6282b0
luke <jjyjjh33@gmail.com>
2024-06-03 15:41:44 +0900

    feat: 관리자 페이지 대시보드 UI완성

--- src/components/units/admin/dashboard/Dashboard.styles.js
+++ src/components/units/admin/dashboard/Dashboard.styles.js
@@ -22,7 +22,8 @@ export const MenuBlock = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
-  gap: 0.488vh;
+  gap: 5px;
+  margin-top: 105px;
 `;
 
 export const MenuItem = styled.div`
@@ -71,14 +72,14 @@ export const ContentsContainer = styled.div`
 
 export const ContentsBlock = styled.div`
   width: 77.986vw;
-  height: 63.086vh;
-  background-color: beige;
+  height: 69.727vh;
+  margin-top: 40px;
 `;
 
 export const Title = styled.div`
   width: 899px;
   height: 68px;
-  font-size: 26px;
+  font-size: 36px;
   font-weight: bold;
 
   border-bottom: 1px solid #E9ECEF;
@@ -127,9 +128,111 @@ export const ContentCount = styled.div`
 `;
 
 export const  GraphContainer = styled.div`
-  width: 100%;
-  height: 380px;
+  width: 1103px;
+  height: 383px;
   border-bottom: 1px solid #E9ECEF;
+
+  display: flex;
+  flex-direction: row;
 `;
 
+export const GraphBlock = styled.div`
+  width: 538px;
+  height: 100%;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GraphItem = styled.div`
+  width: 515px;
+  height: 325px;
+`;
+
+export const GraphTitle = styled.p`
+  width: 150px;
+  height: 50px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const Graph = styled.div`
+  width: 100%;
+  height: 249px;
+  background-color: aqua;
+`;
+
+export const DateBlock = styled.div`
+  width: 100%;
+  height: 32px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const Date = styled.span`
+  width: 40px;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TodayInfoContainer = styled.div`
+  width: 900px;
+  height: 118px;
+  margin-top: 23px;
+  
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CurrentDate = styled.p`
+  width: 100%;
+  height: 50px;
+  font-size: 18px;
+  font-weight: bold;
+  padding-left: 10px;
+  
+`;
+
+export const CurrentInfoBlock = styled.div`
+  width: 100%;
+  height: 45px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #F4F4F5;
+  border-radius: 10px;
+`;
+
+export const CurrentInfoItem = styled.div`
+  width: 113px;
+  height: 50px;
+  font-size: 18px;
+  font-weight: bold;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CurrentInfoNum = styled.div`
+  width: auto;
+  height: 50px;
+  font-size: 15px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
